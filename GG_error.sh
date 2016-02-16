75bbcf5f89c810920d0474b61c3a1487c4b8760f
arasmuc <arkadiuszborucki@gmail.com>
2016-02-16 18:16:28 +0100

    Update GG_error.sh

--- GG_error.sh
+++ GG_error.sh
@@ -12,7 +12,7 @@ ORACLE_HOME="/Oracle/app/oracle/product/db11g"
  
 cd $GOLDEN_GATE_HOME
 
-a=`awk -v d1="$(date --date="-11 min" "+%Y-%m-%d %H:%M:%S")" -v d2="$(date "+%Y-%m-%d %H:%M:%S")" '$0 > d1 && $0 < d2 || $0 ~ d2' ggserr.log | grep ERROR`
+a=`awk -v d1="$(date --date="-11 min" "+%Y-%m-%d %H:%M:%S")" -v d2="$(date "+%Y-%m-%d %H:%M:%S")" '$0 > d1 && $0 < d2 || $0 ~ d2' ggserr.log | grep ERROR|wc -l`
 echo $a
 if [ $a -gt 0 ]; 
 echo "errors occured in last 10 min in ggserr.log"
